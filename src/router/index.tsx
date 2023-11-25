dba3d424c085cda8c55d477af28ab9910d2497a4
donghyun <dh.kim@saigeresearch.ai>
2023-11-25 16:35:42 +0900

    Deploy

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,15 +3,20 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import App from "../App";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <App>
-        <Home />,
-      </App>
-    ),
-  },
-]);
+const basename = process.env.PUBLIC_URL;
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <App>
+          <Home />,
+        </App>
+      ),
+    },
+  ],
+  { basename: basename }
+);
 
 export default router;
