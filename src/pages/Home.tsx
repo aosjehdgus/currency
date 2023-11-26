141c28090585cf79749b57ff650ac39763f905b6
donghyun <dh.kim@saigeresearch.ai>
2023-11-26 22:25:24 +0900

    탭 기능 추가 및 기본 회화 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from "react";
 import {
+  Box,
   IconButton,
   MenuItem,
   Stack,
+  Tab,
+  Tabs,
   TextField,
   Typography,
 } from "@mui/material";
 import axios, { AxiosError } from "axios";
 import ImportExportIcon from "@mui/icons-material/ImportExport";
 import HighlightOffRoundedIcon from "@mui/icons-material/HighlightOffRounded";
+import CurrencyExchangeOutlinedIcon from "@mui/icons-material/CurrencyExchangeOutlined";
+import SmsOutlinedIcon from "@mui/icons-material/SmsOutlined";
 import koImg from "../images/ko.png";
 import jpImg from "../images/jp.png";
 import euImg from "../images/eu.png";
@@ -50,7 +55,110 @@ const currencyMap: CurrencyMap = {
   },
 };
 
+const conversationMap = {
+  실례합니다: {
+    label: "시츠레이시마스",
+  },
+  "한국 서울에서 왔어요": {
+    label: "칸코쿠노 소우루카라 키마시타",
+  },
+  "정말 큰 도움이 됐어요": {
+    label: "혼토니 다스카리마시타",
+  },
+  "몇 살이세요?": {
+    label: "오이쿠츠데스까 ",
+  },
+  "저는 서른살입니다": {
+    label: "와따시와 산주사이 데스",
+  },
+  "일본어는 조금밖에 못합니다": {
+    label: "니혼고와 스코시 시카 데키마세",
+  },
+  "당신의 이름은 무엇입니까?": {
+    label: "아마따노 나마에와 난데스까",
+  },
+  잘먹었습니다: {
+    label: "고치소 사마데시타",
+  },
+  "카드 계산 가능한가요?": {
+    label: "카아도켓사이데키마스카",
+  },
+  "조금 생각해볼게요.": {
+    label: "초쯔토 칸가에마스",
+  },
+  "실례지만 계산해주세요": {
+    label: "스미마셍 오카이케에 오네가이시마스",
+  },
+  "영수증 부탁드립니다.": {
+    label: "레시-토 오네가이시마스",
+  },
+  "사진 좀 찍어주실래요?": {
+    label: "오샤신오 톳테 모라에미스까?",
+  },
+  얼마입니까: {
+    label: "이꾸라 데스까",
+  },
+  "입어봐도 되나요?": {
+    label: "키떼 미떼모 이- 데스까 ",
+  },
+  "딱 맞습니다.": {
+    label: "핍따리데스",
+  },
+  "면세 됩니까?": {
+    label: "멘제이 데끼마스까",
+  },
+  "도와줘서 고마워.": {
+    label: "데츠닷테쿠레테 아리가토",
+  },
+  괜찮습니다: {
+    label: "다이죠부데스",
+  },
+  "이것 있습니까": {
+    label: "코레와 아리마수까",
+  },
+  하나: {
+    label: "히또쯔",
+  },
+  둘: {
+    label: "후따즈",
+  },
+};
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box
+          sx={{
+            background: "rgba(0, 0, 0, 0.01)",
+            boxShadow:
+              "inset -3px -3px 5px rgba(0, 0, 0, 0.05), inset 3px 3px 5px rgba(0, 0, 0, 0.05)",
+          }}
+        >
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
 const Home = () => {
+  const [value, setValue] = useState(0);
+
   const [fromValue, setFromValue] = useState<number>(0);
   const [toValue, setToValue] = useState<number>(0);
 
@@ -62,7 +170,7 @@ const Home = () => {
   const [fromCurrency, setFromCurrency] = useState<Nation>("jpy");
   const [toCurrency, setToCurrency] = useState<Nation>("krw");
 
-  const [isFirstField, setIsFirstField] = useState<boolean | null>(true);
+  const [isFirstField, setIsFirstField] = useState<boolean>(true);
 
   const today = dayjs().format("YYYY-MM-DD");
 
@@ -91,6 +199,10 @@ const Home = () => {
   const initValue = () => {
     setFromValue(0);
     setToValue(0);
+  };
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
   };
 
   useEffect(() => {
@@ -122,204 +234,263 @@ const Home = () => {
   }, [date, isFirstField, fromValue, toValue, fromCurrency, toCurrency]);
 
   return (
-    <Stack
-      width={"100vw"}
-      height={"100vh"}
-      alignItems={"center"}
-      justifyContent={"center"}
-    >
-      <Stack
-        zIndex={1}
-        width={"270px"}
+    <>
+      <Box
         sx={{
-          gap: 3,
-          border: 1,
-          borderRadius: 2,
-          padding: 4,
-          borderColor: "#d3d3d3",
-          background: "rgba(0, 0, 0, 0.01)",
-          boxShadow:
-            "inset -3px -3px 5px rgba(0, 0, 0, 0.03), inset 3px 3px 5px rgba(0, 0, 0, 0.03)",
+          borderBottom: 1,
+          borderColor: "divider",
         }}
       >
-        <Stack
-          direction={"column"}
-          justifyContent={"space-between"}
-          alignItems={"center"}
-          gap={3}
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+          variant="fullWidth"
         >
-          <Typography variant="h4" sx={{ fontWeight: 700, fontSize: "26px" }}>
-            {"Currency Converter"}
-          </Typography>
+          <Tab icon={<CurrencyExchangeOutlinedIcon />} />
+          <Tab icon={<SmsOutlinedIcon />} />
+        </Tabs>
+      </Box>
+
+      <CustomTabPanel value={value} index={0}>
+        <Stack
+          sx={{
+            gap: 3,
+            borderRadius: 2,
+            padding: 4,
+          }}
+        >
           <Stack
-            gap={2}
             direction={"column"}
-            width={"100%"}
+            justifyContent={"space-between"}
             alignItems={"center"}
-            justifyContent={"center"}
+            gap={3}
           >
-            <Stack direction={"column"} alignItems={"center"} gap={2}>
-              <Typography
-                variant="body1"
-                color={"primary"}
-                sx={{
-                  fontWeight: 700,
-                  fontSize: "18px",
-                }}
-              >
-                {`1 ${(isFirstField
-                  ? fromCurrency
-                  : toCurrency
-                ).toUpperCase()} = ${currency?.value} ${(isFirstField
-                  ? toCurrency
-                  : fromCurrency
-                ).toUpperCase()}`}
-              </Typography>
+            <Typography variant="h4" sx={{ fontWeight: 700, fontSize: "26px" }}>
+              {"Currency Converter"}
+            </Typography>
+            <Stack
+              gap={2}
+              direction={"column"}
+              width={"100%"}
+              alignItems={"center"}
+              justifyContent={"center"}
+            >
+              <Stack direction={"column"} alignItems={"center"} gap={2}>
+                <Typography
+                  variant="body1"
+                  color={"primary"}
+                  sx={{
+                    fontWeight: 700,
+                    fontSize: "18px",
+                  }}
+                >
+                  {`1 ${(isFirstField
+                    ? fromCurrency
+                    : toCurrency
+                  ).toUpperCase()} = ${currency?.value} ${(isFirstField
+                    ? toCurrency
+                    : fromCurrency
+                  ).toUpperCase()}`}
+                </Typography>
+              </Stack>
             </Stack>
           </Stack>
-        </Stack>
 
-        <Stack
-          direction={"column"}
-          alignItems={"center"}
-          gap={1}
-          justifyContent={"space-between"}
-        >
-          <TextField
-            fullWidth
-            value={fromValue}
-            onChange={handleFromValue}
-            InputProps={{
-              endAdornment: (
-                <Stack gap={0.5} direction={"row"} alignItems={"center"}>
-                  {fromValue !== 0 && (
-                    <IconButton
-                      size="small"
-                      sx={{ opacity: 0.5 }}
-                      onClick={() => {
+          <Stack
+            direction={"column"}
+            alignItems={"center"}
+            gap={1}
+            justifyContent={"space-between"}
+          >
+            <TextField
+              fullWidth
+              value={fromValue}
+              onChange={handleFromValue}
+              InputProps={{
+                endAdornment: (
+                  <Stack gap={0.5} direction={"row"} alignItems={"center"}>
+                    {fromValue !== 0 && (
+                      <IconButton
+                        size="small"
+                        sx={{ opacity: 0.5 }}
+                        onClick={() => {
+                          initValue();
+                        }}
+                      >
+                        <HighlightOffRoundedIcon />
+                      </IconButton>
+                    )}
+
+                    <TextField
+                      sx={{ minWidth: "80px" }}
+                      select
+                      variant="standard"
+                      value={fromCurrency}
+                      onChange={(e: any) => {
+                        setIsFirstField(true);
+                        setFromCurrency(e.target.value);
                         initValue();
                       }}
                     >
-                      <HighlightOffRoundedIcon />
-                    </IconButton>
-                  )}
+                      {Object.values(currencyMap).map(
+                        ({ title, img }: Example) => {
+                          return (
+                            <MenuItem value={title} key={title}>
+                              <Stack
+                                flexDirection={"row"}
+                                gap={1}
+                                alignItems={"center"}
+                              >
+                                <img src={img} width={16} height={16} />
+                                <Typography>{title.toUpperCase()}</Typography>
+                              </Stack>
+                            </MenuItem>
+                          );
+                        }
+                      )}
+                    </TextField>
+                  </Stack>
+                ),
+              }}
+            />
 
-                  <TextField
-                    sx={{ minWidth: "80px" }}
-                    select
-                    variant="standard"
-                    value={fromCurrency}
-                    onChange={(e: any) => {
-                      setIsFirstField(true);
-                      setFromCurrency(e.target.value);
-                      initValue();
-                    }}
-                  >
-                    {Object.values(currencyMap).map(
-                      ({ title, img }: Example) => {
-                        return (
-                          <MenuItem value={title} key={title}>
-                            <Stack
-                              flexDirection={"row"}
-                              gap={1}
-                              alignItems={"center"}
-                            >
-                              <img src={img} width={16} height={16} />
-                              <Typography>{title.toUpperCase()}</Typography>
-                            </Stack>
-                          </MenuItem>
-                        );
-                      }
+            <ImportExportIcon color="action" />
+
+            <TextField
+              fullWidth
+              value={toValue}
+              onChange={handleToValue}
+              InputProps={{
+                endAdornment: (
+                  <Stack gap={0.5} direction={"row"} alignItems={"center"}>
+                    {fromValue !== 0 && (
+                      <IconButton
+                        size="small"
+                        sx={{ opacity: 0.5 }}
+                        onClick={() => {
+                          initValue();
+                        }}
+                      >
+                        <HighlightOffRoundedIcon />
+                      </IconButton>
                     )}
-                  </TextField>
-                </Stack>
-              ),
-            }}
-          />
-
-          <ImportExportIcon color="action" />
-
-          <TextField
-            fullWidth
-            value={toValue}
-            onChange={handleToValue}
-            InputProps={{
-              endAdornment: (
-                <Stack gap={0.5} direction={"row"} alignItems={"center"}>
-                  {fromValue !== 0 && (
-                    <IconButton
-                      size="small"
-                      sx={{ opacity: 0.5 }}
-                      onClick={() => {
+                    <TextField
+                      sx={{ minWidth: "80px" }}
+                      select
+                      variant="standard"
+                      value={toCurrency}
+                      onChange={(e: any) => {
+                        setIsFirstField(false);
+                        setToCurrency(e.target.value);
                         initValue();
                       }}
                     >
-                      <HighlightOffRoundedIcon />
-                    </IconButton>
-                  )}
-                  <TextField
-                    sx={{ minWidth: "80px" }}
-                    select
-                    variant="standard"
-                    value={toCurrency}
-                    onChange={(e: any) => {
-                      setIsFirstField(false);
-                      setToCurrency(e.target.value);
-                      initValue();
+                      {Object.values(currencyMap).map(
+                        ({ title, img }: Example) => {
+                          return (
+                            <MenuItem value={title} key={title}>
+                              <Stack
+                                gap={1}
+                                flexDirection={"row"}
+                                alignItems={"center"}
+                              >
+                                <img src={img} width={16} height={16} />
+                                <Typography>{title.toUpperCase()}</Typography>
+                              </Stack>
+                            </MenuItem>
+                          );
+                        }
+                      )}
+                    </TextField>
+                  </Stack>
+                ),
+              }}
+            />
+          </Stack>
+          <Stack
+            width={"100%"}
+            direction={"row"}
+            alignItems={"center"}
+            justifyContent={"flex-end"}
+            gap={2}
+          >
+            <Typography
+              variant="body1"
+              sx={{
+                fontWeight: 700,
+                color: "rgba(0, 0, 0, 0.9)",
+                fontSize: "14px",
+              }}
+            >
+              기준 날짜
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{
+                fontWeight: 500,
+                color: "rgba(0, 0, 0, 0.9)",
+                fontSize: "14px",
+              }}
+            >
+              {currency?.date}
+            </Typography>
+          </Stack>
+        </Stack>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <Stack
+          maxHeight={"300px"}
+          overflow={"scroll"}
+          sx={{
+            gap: 3,
+            borderRadius: 2,
+            padding: 4,
+          }}
+        >
+          <Stack alignItems={"center"}>
+            <Typography variant="h4" sx={{ fontWeight: 700, fontSize: "26px" }}>
+              {"기본 회화"}
+            </Typography>
+          </Stack>
+
+          <Stack
+            width={"100%"}
+            direction={"column"}
+            alignItems={"center"}
+            justifyContent={"flex-start"}
+            gap={2}
+          >
+            {Object.entries(conversationMap).map(([key, item]) => {
+              return (
+                <Stack direction={"row"} gap={1}>
+                  <Typography
+                    variant="body1"
+                    sx={{
+                      fontWeight: 700,
+                      color: "rgba(0, 0, 0, 0.9)",
+                      fontSize: "12px",
                     }}
                   >
-                    {Object.values(currencyMap).map(
-                      ({ title, img }: Example) => {
-                        return (
-                          <MenuItem value={title} key={title}>
-                            <Stack
-                              gap={1}
-                              flexDirection={"row"}
-                              alignItems={"center"}
-                            >
-                              <img src={img} width={16} height={16} />
-                              <Typography>{title.toUpperCase()}</Typography>
-                            </Stack>
-                          </MenuItem>
-                        );
-                      }
-                    )}
-                  </TextField>
+                    {key}
+                  </Typography>
+                  <Typography
+                    variant="body1"
+                    sx={{
+                      fontWeight: 700,
+                      color: "rgba(0, 0, 0, 0.7)",
+                      fontSize: "12px",
+                    }}
+                  >
+                    {item.label}
+                  </Typography>
                 </Stack>
-              ),
-            }}
-          />
+              );
+            })}
+          </Stack>
         </Stack>
-        <Stack
-          width={"100%"}
-          direction={"row"}
-          alignItems={"center"}
-          justifyContent={"flex-end"}
-          gap={2}
-        >
-          <Typography
-            variant="body1"
-            sx={{
-              fontWeight: 700,
-              color: "rgba(0, 0, 0, 0.9)",
-              fontSize: "14px",
-            }}
-          >
-            기준 날짜
-          </Typography>
-          <Typography
-            variant="body1"
-            sx={{
-              fontWeight: 500,
-              color: "rgba(0, 0, 0, 0.9)",
-              fontSize: "14px",
-            }}
-          >
-            {currency?.date}
-          </Typography>
-        </Stack>
-      </Stack>
-    </Stack>
+      </CustomTabPanel>
+    </>
   );
 };
 
